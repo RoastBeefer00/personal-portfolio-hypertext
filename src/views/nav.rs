7d3f12efa053f295c9ee6b2367411f5aa55b62c3
Jake Jasmin <roastbeefer000@gmail.com>
2025-06-21 22:01:40 -0600

    fix htmx routing

--- src/views/nav.rs
+++ src/views/nav.rs
@@ -10,25 +10,26 @@ pub fn nav(selected: Page, oob: bool) -> impl Renderable {
         <nav id="nav" class="text-text border-b border-b-blue" hx-swap-oob=true[oob]>
             <ul class="flex flex-row items-center justify-center">
                 @for route in Page::iter() {
-                    <a
-                        href=(route.get_ref())
-                        class={
-                            "flex items-center justify-center w-20 p-2 font-bold hover:rounded-t"
-                            @if route == selected {
-                                " bg-blue rounded-t text-crust hover"
-                            } @else {
-                                " text-text hover:rounded-t hover:bg-lavender hover:text-crust"
-                            }
-                        }
-                        hx-get=(route.get_ref())
-                        hx-target="#page"
-                        hx-swap="innerHTML"
-                        hx-push-url="true"
+                    <li
                     >
-                        <li>
+                        <a
+                            href=(route.get_ref())
+                            class={
+                                "flex items-center justify-center w-20 p-2 font-bold hover:rounded-t"
+                                @if route == selected {
+                                    " bg-blue rounded-t text-crust hover"
+                                } @else {
+                                    " text-text hover:rounded-t hover:bg-lavender hover:text-crust"
+                                }
+                            }
+                            hx-get=(route.get_ref())
+                            hx-target="#page"
+                            hx-swap="innerHTML"
+                            hx-push-url="true"
+                        >
                             (route.to_string())
-                        </li>
-                    </a>
+                        </a>
+                    </li>
                 }
             </ul>
         </nav>
